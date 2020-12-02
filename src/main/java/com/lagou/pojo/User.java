f7effd7c9574ed0bc6b9b23195eac12eb183eb34
liumincong <liumincong-jr@bestpay.com.cn>
2020-12-02 16:57:59 +0800

    multi mapper

--- src/main/java/com/lagou/pojo/User.java
+++ src/main/java/com/lagou/pojo/User.java
@@ -2,12 +2,16 @@ package com.lagou.pojo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class User {
 
     private int id;
 
     private String username;
+
+    private List<Order> orderList;
 
     private Order order;
 }
